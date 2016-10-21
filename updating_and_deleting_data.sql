805ece0143460ad894361f0adcc38e80907fa5f0
Ken T <nyy2008@gmail.com>
2016-10-21 00:30:10 -0400

    Finish chapter up to exercises

--- updating_and_deleting_data.sql
+++ updating_and_deleting_data.sql
@@ -70,6 +70,77 @@ SET winner_date = NULL,
     prize_sent = NULL
 WHERE country_id = 'uk'
 AND prize_winners.human_id = humans.human_id;
+
 SELECT * FROM prize_winners;
 
+UPDATE prize_winners, HUMANS
+SET winner_date = CURDATE()
+WHERE winner_date = NULL
+AND country_id = 'uk'
+AND prize_winners.human_id = humans.human_id
+ORDER BY RAND()
+LIMIT 2;
+### Yields error because usage of UPDATE and ORDER BY clauses
+### cause problems with multiple tables
 
+### Instead use subquery to work around
+
+UPDATE prize_winners
+SET winner_date = CURDATE()
+WHERE winner_date IS NULL
+AND human_id IN
+    (SELECT human_id
+    FROM humans
+    WHERE country_id = 'uk'
+    ORDER BY RAND())
+LIMIT 2;
+
+### 
+ALTER TABLE humans
+DROP COLUMN better_birders_site;
+
+ALTER TABLE humans
+ADD COLUMN better_birders_site TINYINT DEFAULT 0;
+
+INSERT INTO humans
+(formal_title, name_first, name_last, email_address, better_birders_site)
+VALUES
+('Mr', 'Barry', 'Pilson', 'barry@gomail.com', 1),
+('Ms', 'Lexi', 'Hollar', 'alexandra@mysqlresources.com', 1),
+('Mr', 'Ricky', 'Adams', 'ricky@gomail.com', 1)
+ON DUPLICATE KEY
+UPDATE better_birders_site = 2;
+
+INSERT INTO prize_winners
+(human_id)
+SELECT human_id
+FROM humans
+WHERE better_birders_site = 1;
+
+ALTER TABLE humans
+ADD COLUMN possible_duplicate TINYINT DEFAULT 0;
+
+CREATE TEMPORARY TABLE possible_duplicates
+(name_1 varchar(25), name_2 varchar(25));
+
+INSERT INTO possible_duplicates
+SELECT name_first, name_last
+FROM
+    (SELECT name_first, name_last, COUNT(*) AS nbr_entries
+    FROM humans
+    GROUP BY name_first, name_last) AS derived_table
+WHERE nbr_entries > 1;
+
+UPDATE humans, possible_duplicates
+SET possible_duplicate = 1
+WHERE name_first = name_1
+AND name_last = name_2;
+
+DELETE FROM humans, prize_winners
+USING humans JOIN prize_winners
+WHERE name_first = 'Elena'
+AND name_last = 'Bokova'
+AND email_address LIKE '%yahoo.com'
+AND humans.human_id = prize_winners.human_id;
+
+### ALWAYS back up before using DELETE FROM
